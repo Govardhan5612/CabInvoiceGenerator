19b2eccd64bb6ad2ed61d493adfaac7d96fa9236
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-01-08 14:25:34 +0530

    Calculate multiple rides fare

--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import org.bridgelabz.CabInvoice;
+import org.bridgelabz.Ride;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,12 @@ public class CabInvoiceTest {
         CabInvoice cabinvoice = new CabInvoice();
         double totalFare = cabinvoice.getTotalFare(0.2,1);
         Assertions.assertEquals(5,totalFare);
+    }
+    @Test
+    public void givenMultipleRidesShould_ReturnAggregateFare(){
+        CabInvoice cabinvoice = new CabInvoice();
+        Ride[] rides ={new Ride(12.0,3.0),new Ride(14.0,5.0),new Ride(0.1,3.0)};
+        double totalFare = cabinvoice.getTotalFare(rides);
+        Assertions.assertEquals(273,totalFare);
     }
 }
